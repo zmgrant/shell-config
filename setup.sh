977e616e88f3d6db09328065c0391d5aafccea20
zmgrant <zmgrant@gmail.com>
2024-12-02 11:48:34 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 theme='https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json'
 
 if [ "$(uname)" == "Darwin" ]; then
+  echo "Installing on Linux"
   brew install jandedobbeleer/oh-my-posh/oh-my-posh
   shell_conf_file=~/.zshrc
   shell_type=zsh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo "installing on MacOS"
   curl -s https://ohmyposh.dev/install.sh | bash -s
   shell_conf_file=~/.bashrc
   shell_type=bash
