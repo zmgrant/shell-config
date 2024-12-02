1d6fe3a9c3edcabe6d0b508db2911bf42a5d92fb
Zach Grant <zach.grant@skydio.com>
2024-12-02 19:04:45 +0000

    Updated

--- setup.sh
+++ setup.sh
@@ -1,19 +1,75 @@
 #!/bin/bash
 
-theme='https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json'
+# Variables
+THEME="'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json'"  # Replace with your desired theme
+FONT="Meslo"  # Replace with your desired font
 
-if [ "$(uname)" == "Darwin" ]; then
-  echo "Installing on MacOS"
-  brew install jandedobbeleer/oh-my-posh/oh-my-posh
-  shell_conf_file=~/.zshrc
-  shell_type=zsh
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "installing on Linux"
-  curl -s https://ohmyposh.dev/install.sh | bash -s
-  shell_conf_file=~/.bashrc
-  shell_type=bash
-fi
+# Function to install Oh My Posh
+install_oh_my_posh() {
+  echo "Installing Oh My Posh..."
+  curl -s https://ohmyposh.dev/install.sh | bash
+}
+
+# Function to install the specified font
+install_font() {
+  echo "Installing font: $FONT..."
+  oh-my-posh font install "$FONT"
+}
+
+# Function to configure the shell
+configure_shell() {
+  local shell_config=$1
+  local shell_type=$2
   
-oh-my-posh font install meslo
-echo "eval "$(oh-my-posh init $shell_type --config $theme)"" >> $shell_conf_file
-exec $shell_type
+  echo "Configuring $shell_config with Oh My Posh..."
+
+  # Check if oh-my-posh is already configured
+  if grep -q "oh-my-posh init" "$shell_config"; then
+    echo "Oh My Posh already configured. Updating the theme..."
+    sed -i.bak "/oh-my-posh init/ s|--config .*|--config $THEME)\"|" "$shell_config"
+  else
+    echo "Adding Oh My Posh initialization to $shell_config..."
+    echo "eval \"\$(oh-my-posh init $shell_type --config $THEME)\"" >> "$shell_config"
+  fi
+}
+
+# Detect OS
+OS=$(uname -s)
+case "$OS" in
+  Linux)
+    echo "Detected Linux."
+    SHELL_CONFIG="$HOME/.bashrc"
+    SHELL_TYPE="bash"
+    ;;
+
+  Darwin)
+    echo "Detected macOS."
+    SHELL_CONFIG="$HOME/.zshrc"
+    SHELL_TYPE="zsh"
+    ;;
+
+  CYGWIN*|MINGW32*|MSYS*|MINGW*)
+    echo "Detected Windows."
+    SHELL_CONFIG="$HOME/.bashrc"
+    SHELL_TYPE="bash"
+    ;;
+
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+
+install_oh_my_posh
+install_font
+configure_shell "$SHELL_CONFIG" "$SHELL_TYPE"
+
+# Reload the shell config
+echo "Reloading shell configuration..."
+if [[ "$SHELL_TYPE" == "bash" ]]; then
+  source "$SHELL_CONFIG"
+elif [[ "$SHELL_TYPE" == "zsh" ]]; then
+  exec zsh
+fi
+
+echo "Oh My Posh installation and configuration completed!"
