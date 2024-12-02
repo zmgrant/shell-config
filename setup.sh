a1dffca18601675c9b8240c924f3e17efab72113
zmgrant <zmgrant@gmail.com>
2024-12-02 11:36:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-curl -s https://ohmyposh.dev/install.sh | bash -s
-oh-my-posh font install meslo
-echo "eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json')"" >> ~/.bashrc
-exec bash
+theme='https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json'
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install jandedobbeleer/oh-my-posh/oh-my-posh
+  shell_conf_file=~/.zshrc
+  shell_type=zsh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+  shell_conf_file=~/.bashrc
+  shell_type=bash
+  
+  oh-my-posh font install meslo
+  echo "eval "$(oh-my-posh init $shell_type --config $theme)"" >> $shell_conf_file
+  exec $shell_type
