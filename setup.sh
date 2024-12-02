fe4e88a479db1e757d5ef92c0ec2f659f53b5148
zmgrant <zmgrant@gmail.com>
2024-12-02 11:12:55 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
+oh-my-posh font install meslo
+echo "eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json')"" >> ~/.bashrc
+exec bash
